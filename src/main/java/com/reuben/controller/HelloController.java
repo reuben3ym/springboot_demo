0da5000e494e45ffcb89d636eacb7aabdb4e49df
Reuben <yuanmaooooo@gmail.com>
2020-03-06 13:38:24 +0800

    update

--- src/main/java/com/reuben/controller/HelloController.java
+++ src/main/java/com/reuben/controller/HelloController.java
@@ -1,14 +1,12 @@
 package com.reuben.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 public class HelloController {
     @GetMapping("/hello")
-    @ResponseBody
     public String hello() {
         return "hello";
     }
