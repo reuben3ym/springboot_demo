8b25cb381d26bdbbf916f9cb46580bf777d38382
Reuben <yuanmaooooo@gmail.com>
2020-03-07 16:33:37 +0800

    test defalut user

--- src/test/java/com/reuben/springboot_demo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/reuben/springboot_demo/SpringbootDemoApplicationTests.java
@@ -1,14 +1,21 @@
 package com.reuben.springboot_demo;
 
+import com.reuben.bean.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootDemoApplicationTests {
-
+    @Autowired
+    private User user;
 	@Test
 	void contextLoads() {
 		System.out.println("this is a test");
 	}
+	@Test
+	void test_default_user(){
+        System.out.println(user);
+    }
 
 }
