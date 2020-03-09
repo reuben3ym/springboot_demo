c47396e5c150b09187e6bab7c7d225faaf56dc4f
Reuben <yuanmaooooo@gmail.com>
2020-03-09 10:15:21 +0800

    Validated and add email

--- src/main/java/com/reuben/pojo/User.java
+++ src/main/java/com/reuben/pojo/User.java
@@ -4,32 +4,48 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-@ApiModel(value="user对象",description="用户对象user")
+
+@ApiModel(value = "user对象", description = "用户对象user")
 @Component
 @ConfigurationProperties(prefix = "user")
+@Validated
 public class User implements Serializable {
-    @ApiModelProperty(value="用户名",name="userName",example="reuben")
+    @ApiModelProperty(value = "用户名", name = "userName", example = "reuben")
+    @NotBlank //@NotNull  @NotEmpty
     private String userName;
-    @ApiModelProperty(value="密码",name="userPassword",example="reuben")
+
+    @NotBlank
+    @ApiModelProperty(value = "密码", name = "userPassword", example = "reuben")
     private String userPassword;
+
+    @ApiModelProperty(value = "邮箱", name = "email", example = "reuben@mail.com")
+    @NotBlank
+    @Email
+    private String email;
 
     public User() {
 
     }
 
-    public User(String userName, String userPassword) {
+    public User(@NotNull String userName, @NotNull String userPassword, @Email String email) {
         this.userName = userName;
         this.userPassword = userPassword;
+        this.email = email;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "userName='" + userName + '\'' +
-                ", userPassword='" + userPassword + '\'' +
-                '}';
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getUserName() {
@@ -46,6 +62,15 @@ public class User implements Serializable {
 
     public void setUserPassword(String userPassword) {
         this.userPassword = userPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 
 
