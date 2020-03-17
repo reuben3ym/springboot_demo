a7e5f82fd01c3ac446fde953d9f64a79d7a0c2ac
Reuben <yuanmaooooo@gmail.com>
2020-03-17 19:42:03 +0800

    update

--- src/main/java/com/reuben/pojo/User.java
+++ src/main/java/com/reuben/pojo/User.java
@@ -2,76 +2,60 @@ package com.reuben.pojo;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
 import java.io.Serializable;
 
 @ApiModel(value = "user对象", description = "用户对象user")
 @Component
 @ConfigurationProperties(prefix = "user")
 @Validated
+@Data//get,set方法
+@AllArgsConstructor//全参构造
+@NoArgsConstructor//无参构造
+//jpa
+@Entity //标注实体类
+@Table(name = "tb_user")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//主键自增
+    @Column(name = "id")
+    private Integer userId;
+
     @ApiModelProperty(value = "用户名", name = "userName", example = "reuben")
-    @NotBlank //@NotNull  @NotEmpty
+    @NotBlank
+    @Column(name = "user_name")
     private String userName;
 
-    @NotBlank
+
     @ApiModelProperty(value = "密码", name = "userPassword", example = "reuben")
+    @NotBlank
+    @Column(name = "user_password")
     private String userPassword;
 
     @ApiModelProperty(value = "邮箱", name = "email", example = "reuben@mail.com")
     @NotBlank
     @Email
+    @Column(name = "email")
     private String email;
 
-    public User() {
+    @Column(name = "isdel")
+    private String isdel;//是否删除:1删除，0未删除
 
-    }
 
-    public User(@NotNull String userName, @NotNull String userPassword, @Email String email) {
+    public User(@NotBlank String userName, @NotBlank String userPassword, @NotBlank @Email String email) {
         this.userName = userName;
         this.userPassword = userPassword;
         this.email = email;
+        this.isdel = "0";//默认0，未被删除
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userName='" + userName + '\'' +
-                ", userPassword='" + userPassword + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
-
-
 }
