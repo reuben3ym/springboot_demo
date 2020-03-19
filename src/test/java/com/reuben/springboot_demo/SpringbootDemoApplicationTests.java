4f2a8dfcb9727ab89b2648175f5a7021539f7cb5
Reuben <yuanmaooooo@gmail.com>
2020-03-19 11:27:19 +0800

    add MockMvc init()

--- src/test/java/com/reuben/springboot_demo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/reuben/springboot_demo/SpringbootDemoApplicationTests.java
@@ -5,16 +5,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class SpringbootDemoApplicationTests {
+    protected MockMvc mvc;
+    @Autowired
+    private WebApplicationContext context;
 
     @Before
     public void init() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
         log.info("--------start testing------");
     }
 
