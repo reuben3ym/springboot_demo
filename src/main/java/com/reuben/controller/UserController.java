f11c41cbb07399f8707b8007ab6b5319f9b07916
Reuben <yuanmaooooo@gmail.com>
2020-03-19 14:24:12 +0800

    Change to restful style

--- src/main/java/com/reuben/controller/UserController.java
+++ src/main/java/com/reuben/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.reuben.controller;
 
-import com.reuben.dao.UserDao;
 import com.reuben.pojo.User;
 import com.reuben.service.UserService;
 import io.swagger.annotations.Api;
@@ -10,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/user")
@@ -20,15 +20,14 @@ public class UserController {
     private User user_default;
 
     @Autowired
-    private UserDao userDao;
-
-    @Autowired
     private UserService userService;
 
     @PostMapping("/testPost")
     @ApiOperation(value = "post提交User", notes = "需要User作为参数", httpMethod = "POST")
-    public String testPost(@ApiParam(value = "用户名、密码", required = true) User user) {
-        return "Success! Hello!" + user.getUserName() + ",your password id" + user.getUserPassword();
+    public String testPost(@ApiParam(value = "userName:用户名、userPassword:密码", required = true)
+                           @RequestBody User user) {
+        System.out.println(user);
+        return "Success! Hello!" + user.getUserName() + ",your password is:" + user.getUserPassword();
     }
 
     @GetMapping("/getDefaultUser")
@@ -39,25 +38,36 @@ public class UserController {
 
     @PostMapping("/addUser")
     @ApiOperation(value = "添加User", notes = "需要User作为参数", httpMethod = "POST")
-    public String addUser(@ApiParam(value = "用户名、密码、邮箱", required = true) User user) {
-        if (null != user && 0 != user.getUserName().length()) {
-            user.setIsdel("0");//默认未删除
-            userDao.save(user);
-            return "save Success";
-        } else
-            return "false";
+    public String addUser(@ApiParam(value = "userName:用户名、userPassword:密码、email:邮箱", required = true)
+                          @RequestBody User user) {
+        return userService.save(user);
     }
 
     @GetMapping("/getAllUser")
     @ApiOperation(value = "获取所有User", notes = "无需参数", httpMethod = "GET")
     public String getAllUser() {
-        return userDao.findAll().toString();
+        return userService.findAll();
+    }
+
+    @PostMapping("/findByUserName")
+    @ApiOperation(value = "通过userName获取User", notes = "需要userName作为参数", httpMethod = "POST")
+    public String findByUserName(@ApiParam(value = "用户名", required = true)
+                                 @RequestBody String name) {
+        return userService.findByUserName(name);
     }
 
     @PostMapping("/deleteUserById")
     @ApiOperation(value = "通过id删除User", notes = "需要id作为参数", httpMethod = "POST")
-    public String deleUserById(Integer id) {
+    public String deleUserById(@ApiParam(value = "用户id", required = true)
+                               @RequestBody Integer id) {
         return userService.deleteByUserId(id);
+    }
+
+    @PostMapping("/update")
+    @ApiOperation(value = "修改User", notes = "需要User作为参数", httpMethod = "POST")
+    public String update(@ApiParam(value = "userId:id、userName:用户名、userPassword:密码、email:邮箱", required = true)
+                         @RequestBody User user) {
+        return userService.update(user);
     }
 
 
