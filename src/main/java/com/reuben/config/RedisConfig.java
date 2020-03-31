0275edc7d0b0b7eaf907f0b2805b53872e7d4181
Reuben <yuanmaooooo@gmail.com>
2020-03-31 14:11:00 +0800

    redis Configuration

---
+++ src/main/java/com/reuben/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.reuben.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @program: springboot_demo
+ * @description: redis配置类
+ * @author: reuben
+ * @create: 2020-03-31 09:49
+ **/
+@Configuration
+public class RedisConfig {
+    /**
+     * springboot2.x 使用LettuceConnectionFactory 代替 RedisConnectionFactory
+     * application.yml配置基本信息后,springboot2.x  RedisAutoConfiguration能够自动装配
+     * LettuceConnectionFactory 和 RedisConnectionFactory 及其 RedisTemplate
+     * @Param: [lettuceConnectionFactory]
+     * @return: org.springframework.data.redis.core.RedisTemplate<java.lang.String,java.lang.Object>
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        return redisTemplate;
+    }
+}
